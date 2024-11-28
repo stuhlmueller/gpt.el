f2400e65f24b7efebb203319c29c135b5f825faf
Anselm Coogan <anselm.coogan@icloud.com>
2024-11-28 13:40:57 +0100

    Update package description

--- gpt.el
+++ gpt.el
@@ -1,30 +1,25 @@
 ;;; gpt.el --- Run instruction-following language models -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022 Andreas Stuhlmueller
+;; Copyright (C) 2022 Andreas Stuhlmueller, 2024 Anselm Coogan
 
 ;; Author: Andreas Stuhlmueller <andreas@ought.org>
-;; Version: 1.1
+;; Maintainer: Anselm Coogan <anselm.coogan@gmail.com>
+;; Version: 1.0
 ;; Keywords: openai, anthropic, claude, language, copilot, convenience, tools
-;; URL: https://github.com/stuhlmueller/gpt.el
+;; URL: https://github.com/AnselmC/gpt.el
 ;; License: MIT
 ;; SPDX-License-Identifier: MIT
 ;; Package-Requires: ((emacs "24.4"))
 
 ;;; Commentary:
 
-;; This package defines a set of functions and variables for running
-;; instruction-following language models like GPT-4 and Claude 3.5
-;; Sonnet.  It allows the user to enter a command with history and
-;; completion, and optionally use the current region as input.  The
-;; output of the command is displayed in a temporary buffer with the
-;; same major mode as the original buffer.  The output is streamed as
-;; it is produced by the GPT process.  The user can enter a follow-up
-;; command in the output buffer, which will provide the output, the
-;; follow-up command to GPT as a new prompt.  The follow-up output
-;; will be appended to the output buffer.  The user can view and
-;; export the command history to a file.
+;; This package is a fork of https://github.com/stuhlmueller/gpt.el
+;; It aims to provide a more comprehensive integration of GPT support in emacs.
+;; Features include: multiple chats with different models, completion at point,
+;; and transformations of regions.
 
 (require 'savehist)
+(require 'project)
 
 (savehist-mode 1)
 
