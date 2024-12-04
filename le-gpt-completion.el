065ce25dbe5a5be2b3277b48bfb82478c2da1179
Anselm Coogan <anselm.coogan@icloud.com>
2024-12-04 16:35:01 +0100

    remove overlay entirely

--- le-gpt-completion.el
+++ le-gpt-completion.el
@@ -11,10 +11,6 @@
 (require 'le-gpt-core)
 (require 'le-gpt-project)
 
-(defface le-gpt-completion-preview-face
-  '((t :inherit current :underline t :weight bold))
-  "Face for previewing code completions.")
-
 (defcustom le-gpt-complete-at-point-instructions "Provide a short completion to be inserted at <cursor>. Only provide the completion, no commentary, no quotes, no code blocks. Your response will directly be inserted."
   "The instructions for gpt to perform completion at point without any noise."
   :type 'string
@@ -25,7 +21,6 @@
 If TEMP-CONTEXT-FILES is non-nil, prompt for context files.
 The generated completion is displayed directly in buffer."
   (let* ((start-point (point))
-         (overlay (make-overlay start-point start-point))
          (buffer-content (buffer-substring-no-properties (point-min) start-point))
          (buffer-rest (buffer-substring-no-properties start-point (point-max)))
          (project-context (le-gpt--get-project-context temp-context-files))
@@ -34,25 +29,13 @@ The generated completion is displayed directly in buffer."
          (prompt-file (le-gpt--create-prompt-file prompt))
          (insertion-marker (make-marker))
          (process (le-gpt--make-process prompt-file nil)))
-    (overlay-put overlay 'face 'le-gpt-completion-preview-face)
     (set-marker insertion-marker (point))
     (set-process-filter process (lambda (proc string)
                                   (ignore proc)
                                   (save-excursion
                                     (goto-char insertion-marker)
                                     (insert string)
-                                    (move-overlay overlay start-point (point))
-                                    (set-marker insertion-marker (point)))))
-    (set-process-filter process (lambda (proc string)
-                                  (ignore proc)
-                                  (save-excursion
-                                    (goto-char insertion-marker)
-                                    (insert string)
-                                    (move-overlay overlay start-point (point))
-                                    (set-marker insertion-marker (point)))))
-    (while (process-live-p process)
-      (accept-process-output process))
-    (delete-overlay overlay)))
+                                    (set-marker insertion-marker (point)))))))
 
 (provide 'le-gpt-completion)
 ;;; le-gpt-completion.el ends here
