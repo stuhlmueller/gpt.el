ef1a60c09cfb4d5c0614e4c755e0d374e322995c
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2025-04-11 18:55:15 -0700

    Update version to 2.0 in all relevant files

--- gpt-mode.el
+++ gpt-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2022 Andreas Stuhlmueller
 
 ;; Author: Andreas Stuhlmueller <emacs@stuhlmueller.org>
-;; Version: 1.4
+;; Version: 2.0
 ;; Keywords: openai, anthropic, claude, language, copilot, convenience, tools
 ;; URL: https://github.com/stuhlmueller/gpt.el
 ;; License: MIT
